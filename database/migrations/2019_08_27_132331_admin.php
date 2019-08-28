a397a24133d2001d6bdeb95357d096bb378254ec
loveaoe33 <loveaoe33@gmail.com>
2019-08-29 00:28:30 +0800

    Revert "測試二號"
    This reverts commit 43c135a1bf14620d161d529d4f75e53cb6fbb1cb.

--- database/migrations/2019_08_27_132331_admin.php
+++ database/migrations/2019_08_27_132331_admin.php
@@ -19,7 +19,7 @@ class Admin extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('test22');
+            $table->string('test');
             $table->rememberToken();
             $table->timestamps();
         });
